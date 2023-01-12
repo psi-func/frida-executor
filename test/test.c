60b45816f1ad646c2321b84233fc6f94d6e112f8
psi_foo <shalaginmx998@yandex.ru>
2023-01-12 13:33:35 +0300

    update test

--- test/test.c
+++ test/test.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <assert.h>
 
-int target_func(char* buf, int size) {
-    
+int target_func(char* bf, int size) {
+  char* buf = (char*)malloc(size);
+  memcpy(buf, bf, size);
+
   switch (buf[0]) {
     case 1:
       if(buf[1]=='\x44') {
@@ -19,15 +23,21 @@ int target_func(char* buf, int size) {
         sleep(2);
       }
       break;
+    case  'a':
+      buf[2] = 23;
+      if (buf[1] == 'b') return 20;
     default:
       break;
   }
 
+  free(buf);
   return 1;
 }
 
-int main() {
-
+int main(int argc, char* argv[]) {
+  if (argc < 2) exit(-1);
+  
+  target_func(argv[1], strlen(argv[1]));
   while (1)
     sleep(1);
 
